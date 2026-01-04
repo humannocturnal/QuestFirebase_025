8e883cfabfb0a9c2641104691a69ac98c1120d05
humannocturnal <syaputrar374@gmail.com>
2026-01-04 11:16:45 +0700

    menambahkan data class Siswa dan DetailSiswa

--- app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -1,2 +1,36 @@
 package com.example.myfirebase.modeldata
 
+data class Siswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+data class DetailSiswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+/* Mapper */
+fun DetailSiswa.toDataSiswa(): Siswa =
+    Siswa(id, nama, alamat, telpon)
+
+fun Siswa.toDetailSiswa(): DetailSiswa =
+    DetailSiswa(id, nama, alamat, telpon)
+
+/* UI State */
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+/* Convert data ke UI State */
+fun Siswa.toUIStateSiswa(
+    isEntryValid: Boolean = false
+): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
