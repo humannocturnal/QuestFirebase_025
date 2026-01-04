a451b4be4be678b40a81a9874af71d9ea1547985
humannocturnal <syaputrar374@gmail.com>
2026-01-04 11:13:24 +0700

    buat TopAppBar terpusat untuk halaman siswa

--- app/src/main/java/com/example/myfirebase/view/SiswaTopAppBar.kt
+++ app/src/main/java/com/example/myfirebase/view/SiswaTopAppBar.kt
@@ -1,2 +1,34 @@
 package com.example.myfirebase.view
 
+import android.graphics.drawable.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(title)
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        }
+    )
+}
