c67fbb63aab7b7521d7d9cf126419f4a7ebb41ef
humannocturnal <syaputrar374@gmail.com>
2026-01-04 12:04:08 +0700

    tambah ViewModelFactory dan penyedia repository

--- app/src/main/java/com/example/myfirebase/ViewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/ViewModel/PenyediaViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.myfirebase.ViewModel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myfirebase.repositori.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa() : AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
+        }
+    }
+}
